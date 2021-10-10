8edfce2860a610e9d9b7578327c6c10c91920b63
Daniel Chin <daniel.chin@nyu.edu>
2021-10-10 01:09:09 -0400

    func

--- runtime.py
+++ runtime.py
@@ -1,3 +1,6 @@
+from typing import List, Dict
+from parser import Sequence, FunctionDefinition, FunctionArg
+
 class Thing:
     def __init__(self) -> None:
         self.type : Type = None
@@ -5,7 +8,8 @@ class Thing:
 
         # if it is a function
         self.environment = []
-        self.mst = None
+        self.mst : FunctionDefinition = None
+        self.default_args = {}
 
         self.builtin_repr = None
     
@@ -31,3 +35,58 @@ class UserType(Type):
 class BuiltinType(Type): pass
 class Function(BuiltinType): pass
 class ThingTemplate(BuiltinType): pass
+
+class Helicopter(Exception): 
+    def __init__(self, content = None):
+        super().__init__()
+        self.content : Thing = content
+        self.stack = []
+
+class Environment(list):
+    def assign(self, name, value : Thing):
+        self[-1][name] = value
+    
+    def read(self, name):
+        for namespace in reversed(self):
+            try:
+                return namespace[name]
+            except KeyError:
+                pass
+        raise Helicopter(NameError(f'name "{name}" is not defined.'))
+        # No! this needs to be an in-user exception. todo
+
+def executeSequence(sequence : Sequence, environment):
+    ...
+
+def executeFunction(
+    func : Thing, args : List[Thing], 
+    keyword_args : Dict[str, Thing], 
+):
+    argument_namespace = {}
+    arg_names = [x.name for x in func.mst._def[1:]]
+    for i, thing in enumerate(args):
+        try:
+            name = arg_names[i]
+        except IndexError:
+            raise Helicopter
+            # todo
+        argument_namespace[name] = thing
+    if func.mst._def[i + 2].value is None:
+        # not enough args. 
+        ...
+    for name, thing in keyword_args.items():
+        if name in arg_names:
+            if name in argument_namespace:
+                # multiple values for argument
+                ...
+            argument_namespace[name] = thing
+        else:
+            # unknown argument
+            ...
+    argument_namespace = {
+        **func.default_args, **argument_namespace, 
+    }
+    func.environment.append(argument_namespace)
+
+def executeScript():
+    ...
