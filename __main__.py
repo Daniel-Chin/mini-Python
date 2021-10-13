94a6400e92750f3aab18e03510a80162ce4d424b
Daniel Chin <daniel.chin@nyu.edu>
2021-10-13 16:35:28 -0400

    repl/file entry

--- __main__.py
+++ __main__.py
@@ -1,11 +1,10 @@
 import os
 import argparse
-from builtin import reprString
-from runtime import RunTime, Helicopter
+from runtime import RunTime, Helicopter, reprString
 
 def runScript(entry_filename):
     dir_location, filename = os.path.split(entry_filename)
-    name = os.path.splitext(filename)
+    name, _ = os.path.splitext(filename)
     runTime = RunTime(dir_location)
     try:
         runTime.imPort(name, '__main__')
@@ -13,7 +12,7 @@ def runScript(entry_filename):
         printStackTrace(h)
 
 def printStackTrace(helicopter : Helicopter):
-    print('Traceback (most recent call last):')
+    print('miniPy traceback (most recent call last):')
     while True:
         for filename, line_number, label in helicopter.stack:
             acc = line_number
@@ -39,13 +38,20 @@ def main():
         description='miniPy', 
     )
     parser.add_argument(
-        'scriptname', type=str, nargs=1,
+        'scriptname', type=str, nargs='?', default=None, 
         help='filename of the miniPy script to be executed', 
     )
+    scriptname = parser.parse_args().scriptname
+    scriptname = 'test.minipy'
+    if scriptname is None:
+        repl()
+    else:
+        filename = os.path.abspath(scriptname)
+        with open(filename, 'r') as _:
+            pass    # just to check permission, isfile...
+        runScript(filename)
 
-    args = parser.parse_args()
-    from console import console
-    console({**globals(), **locals()})
-    args
+def repl():
+    print('Under construction')
 
 main()
